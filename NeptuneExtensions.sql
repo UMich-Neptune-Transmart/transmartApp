3610c798e04168b2ccdd96550a7d0b689493b25d
Terry-Weymouth <weymouth@umich.edu>
2015-12-15 14:41:43 -0500

    added qualitiers to new version of modifier_dimension_view in NepruneExtensions.sql

--- NeptuneExtensions.sql
+++ NeptuneExtensions.sql
@@ -8,7 +8,12 @@ CREATE TABLE "i2b2demodata"."neptune_xtrials_metadata" (
 -- Modify view to include Across Trials tooltips
 
 CREATE OR REPLACE VIEW "i2b2demodata"."modifier_dimension_view" AS 
-SELECT md.modifier_path, md.modifier_cd, md.name_char, md.modifier_blob, md.update_date, md.download_date, md.import_date, md.sourcesystem_cd, md.upload_id, md.modifier_level, md.modifier_node_type, mm.valtype_cd, mm.std_units, mm.visit_ind, nm.c_tooltip FROM ((modifier_dimension md LEFT JOIN modifier_metadata mm ON (((md.modifier_cd)::text = (mm.modifier_cd)::text))) LEFT JOIN neptune_xtrials_metadata nm ON (((md.modifier_cd)::text = (nm.modifier_cd)::text)));
+SELECT md.modifier_path, md.modifier_cd, md.name_char, md.modifier_blob, md.update_date, md.download_date, md.import_date,
+md.sourcesystem_cd, md.upload_id, md.modifier_level, md.modifier_node_type, mm.valtype_cd, mm.std_units,
+mm.visit_ind, nm.c_tooltip
+FROM ((i2b2demodata.modifier_dimension md
+  LEFT JOIN i2b2demodata.modifier_metadata mm ON (((md.modifier_cd)::text = (mm.modifier_cd)::text)))
+  LEFT JOIN i2b2demodata.neptune_xtrials_metadata nm ON (((md.modifier_cd)::text = (nm.modifier_cd)::text)));
 
 ALTER TABLE "i2b2demodata"."modifier_dimension_view" OWNER TO "i2b2demodata";
 
